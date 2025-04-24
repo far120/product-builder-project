ff1e91e84e1e7ca3036f2006fe37d94b6bbdd60e
far120 <tatafar2018@gmail.com>
2025-04-24 17:39:21 +0200

    v2

--- src/Ui/utils/functions.ts
+++ src/Ui/utils/functions.ts
@@ -4,7 +4,7 @@
  * @param {number} [max=50] - The maximum length before truncation.
  * @returns The sliced text, with an ellipsis (...) appended if truncated.
  */
-export function txtSlicer(txt: string, max: number = 80) {
+export function txtSlicer(txt: string, max: number = 50) {
   if (txt.length >= max) return `${txt.slice(0, max)}...`;
   return txt;
 }
--- src/validation/index.ts
+++ src/validation/index.ts
@@ -6,8 +6,8 @@ export const productValidation = (product: { title: string; description: string;
     price: "",
   };
   const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(product.imageURL);
-  if (!product.title.trim() || product.title.length < 10 || product.title.length > 80) {
-    errors.title = "Product title must be between 10 and 80 characters!";
+  if (!product.title.trim() || product.title.length < 10 || product.title.length > 30) {
+    errors.title = "Product title must be between 10 and 30 characters!";
   }
   if (!product.description.trim() || product.description.length < 10 || product.description.length > 900) {
     errors.description = "Product description must be between 10 and 900 characters!";
